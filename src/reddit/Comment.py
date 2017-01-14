3558b6676729fd38cff3b4f33a9e36d677bc009c
Hayden White <hsw33@uclive.ac.nz>
2017-01-14 19:20:52 +1300

    Add explanation to validation function since it confused me

--- src/reddit/Comment.py
+++ src/reddit/Comment.py
@@ -1,6 +1,14 @@
 from const import *
 
 def validate(state, comment):
+    '''Check that the comment meets the following conditions:
+    - contains the string `+correct`
+    - is a comment on the current round
+    - is posted by either the current host or a mod
+    - is not a top-level comment
+    - the person being +corrected is not any of the bots, the current host, or the person giving the +correct (who may not be the host)
+    '''
+
     return CORRECT_PATTERN.match(comment.body) and \
             comment.submission.id == state.roundId and \
             comment.author.name in state.mods.union({state.currentHost}) and \
