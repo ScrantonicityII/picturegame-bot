c96cd1f540e743652e016d3a09e2da05d63cf395
Hayden White <hsw33@uclive.ac.nz>
2017-02-28 22:01:40 +1300

    Remove unnecessary sleeps from modmail deleter

--- src/reddit/Mail.py
+++ src/reddit/Mail.py
@@ -4,11 +4,9 @@ from const import *
 
 def archiveModMail(state):
     conversations = state.reddit.get(CONVS_URL)["conversations"]
-    sleep(2)
 
     for convId in conversations:
         conv = conversations[convId]
         if conv["subject"] == "you are an approved submitter" and \
                 len(conv["authors"]) == 1 and conv["authors"][0]["name"] == state.config["botName"]:
             state.reddit.post(ARCHIVE_URL.format(convId))
-            sleep(2)
